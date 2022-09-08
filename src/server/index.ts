64a784121830832b96c9e6f7c06469cc1f579899
Danilo <danilomaioli@gmail.com>
2022-09-07 21:18:06 -0300

    Base project / template - fixed

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,43 +1,35 @@
-import express from 'express';
-import http from 'http';
-import socketio from 'socket.io';
-import path from 'path';
+import express from 'express'
+import http from 'http'
+import socketio from 'socket.io'
+import path from 'path'
 
-const isDevelopment = (process.env.NODE_ENV || "development").trim() === 'development';
-const port = process.env.PORT || 3000;
+const isDevelopment = (process.env.NODE_ENV || "development").trim() === 'development'
+const port = process.env.PORT || 3000
 const io: socketio.Server = new socketio.Server()
-const app: express.Application = express();
-const server: http.Server = http.createServer(app);
+const app: express.Application = express()
+const server: http.Server = http.createServer(app)
 
-function main()
-{
-  console.log("server.ts | (ignoring)isDevelopment=", isDevelopment);
-  //runCPUTest();
-  setupExpress();
-  //const game = new Game();
-  //game.start();
+function main() {
+    console.log("[index] isDevelopment =", isDevelopment)
+    //runCPUTest()
+    setupExpress()
+    //const game = new Game()
+    //game.start()
 }
 
-function setupExpress()
-{
-  //path: '/socket',
-  io.attach(server, {cors: { origin: '*' }});
-  //app.use(express.static(path.join(__dirname, "..", "..", "dist")));
-  app.use(express.static(path.join(__dirname, "..", "..", "public")));
-  server.listen(port, () => console.log(`Express web server started: http://localhost:${port}`));
+function setupExpress() {
+    io.attach(server, {cors: { origin: '*' }}) //path: '/socket',
+    app.use(express.static(path.join(__dirname, "..", "..", "public")))
+    server.listen(port, () => console.log(`[index] Express web server started: http://localhost:${port}`))
 }
 
-function runCPUTest()
-{
-  var os = require('os-utils');
-  setInterval(() => {
-    os.cpuUsage(function(v){
-        console.log( 'CPU Usage (%): ' + v );
-    });
-  }, 1000)
+function runCPUTest() {
+    const os = require('os-utils');
+    setInterval(() => {
+        os.cpuUsage(function(v) {
+            console.log('[index] CPU Usage (%): ' + v)
+        })
+    }, 1000)
 }
 
-main();
-
-
-
+main()
